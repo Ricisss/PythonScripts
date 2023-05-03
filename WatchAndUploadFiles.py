e823693689b4da67ae7409a524757388b3add007
Richard Smith <qpalymmm@gmail.com>
2023-05-03 10:57:51 +0200

    ya

--- WatchAndUploadFiles.py
+++ WatchAndUploadFiles.py
@@ -2,20 +2,22 @@ import time
 import os
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+from OneDrive import uploadFile
 
 path = 'D:\python'
 
-def WatchPath(WATCH_PATH):
+def WatchPath(WATCH_PATH, APP_ID):
     class NewFileHandler(FileSystemEventHandler):
         def on_created(self, event):
             if not event.is_directory:
-                filename = event.src_path
-                print(f"New file created: {filename}")
-                process_new_file(filename)
+                file_path = event.src_path
+                print(f"New file created: {file_path}")
+                process_new_file(file_path)
 
-    def process_new_file(filename):
+    def process_new_file(file_path):
         # Do whatever you need to do with the new file here
-        print(f"Processing new file: {filename}")
+        print(f"Processing new file: {file_path}")
+        uploadFile(file_path, APP_ID)
 
     if __name__ == "__main__":
         event_handler = NewFileHandler()
@@ -27,7 +29,6 @@ def WatchPath(WATCH_PATH):
             while True:
                 time.sleep(10)
         except KeyboardInterrupt:
-            observer.stop()
+            #observer.stop()
+            print("Hupsík dupsík")
         observer.join()
-
-WatchPath(path)
