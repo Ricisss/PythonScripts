f2469675f0efbab6962a7cde9e0e01c74b5295ee
Richard Smith <qpalymmm@gmail.com>
2023-05-04 09:41:28 +0200

    still aint working

--- OneDrive.py
+++ OneDrive.py
@@ -8,10 +8,13 @@ import shutil
 import time
 from tqdm.auto import tqdm
 from tqdm.utils import CallbackIOWrapper
+import concurrent.futures
 
 GRAPH_API_ENDPOINT = 'https://graph.microsoft.com/v1.0'
 SCOPES = ["User.ReadWrite"]
 remote_folder_name = "test"
+#hunk_size = 327680 * 192
+chunk_size = 327680 * 10
 
 def generate_access_token(scopes):
     with open('APP_ID', 'r') as file:
@@ -153,7 +156,9 @@ def uploadLargeFile(file_path, folder_id):
                 chunkl_data_upload_status = requests.put(upload_url,headers=headers,data=chunk_data)
 
                 if "createdBy" in chunkl_data_upload_status.json():
-                    print("DONE")
+                    speed_estimate = (chunk_size * counter) / (time.time()-time_start) / 1024 / 1024
+                    speed_estimate = round(speed_estimate, 2)
+                    print(f"DONE in { round(time.time()-time_start, 2)}s {speed_estimate}MiB/s")
                 else:
                     counter += 1   
                     speed_estimate = (chunk_size * counter) / (time.time()-time_start) / 1024 / 1024
@@ -164,3 +169,89 @@ def uploadLargeFile(file_path, folder_id):
             except Exception as e:
                 print(e)
                 break
+                
+def uploadLargeFileConcurent(file_path, folder_id, MAX_THREADS):
+    global upload
+    #GET SESSION URL
+    access_token = generate_access_token(SCOPES)
+    headers = {
+        'Authorization': 'Bearer ' + access_token['access_token']
+    }
+
+    file_name = os.path.basename(file_path)
+
+    if not os.path.exists(file_path):
+        raise Exception(f"{file_name} not found")
+
+    #with open(file_path, 'rb') as upload:
+    #    media_content = upload.read()
+
+    request_body={
+        "@microsoft.graph.conflictBehavior": "rename",
+        "description": "description",
+        "fileSystemInfo": { "@odata.type": "microsoft.graph.fileSystemInfo" },
+        "name": file_name
+    }
+
+
+    response_upload_session = requests.post(
+        GRAPH_API_ENDPOINT + f'/me/drive/items/{folder_id}:/{file_name}:/createUploadSession',
+        headers = headers,
+        json=request_body
+    )
+
+    #UPLOAD DATA TO SESSION
+
+    with open(file_path, 'rb') as upload:
+        print(f"Chunk size: {chunk_size/1024/1024}MiB")
+        global chunk_number
+        global chunk_leftover 
+        global upload_url
+        global total_file_size
+        global time_start
+        
+        time_start = time.time();
+        total_file_size = os.path.getsize(file_path)
+        chunk_number = total_file_size // chunk_size
+        chunk_leftover = total_file_size - (chunk_size * chunk_number)
+        upload_url = response_upload_session.json()['uploadUrl']
+        
+        try:
+            with concurrent.futures.ThreadPoolExecutor(max_workers=MAX_THREADS) as executor:
+                res = executor.map(uploadChunk,  range(chunk_number+1))
+                
+            speed_estimate = (chunk_size * chunk_number) / (time.time()-time_start) / 1024 / 1024
+            speed_estimate = round(speed_estimate, 2)
+            print(f"DONE in { round(time.time()-time_start, 2)}s {speed_estimate}MiB/s")
+            
+        except Exception as e:
+            print(e)
+    
+
+
+        
+def uploadChunk(i):
+    try:        
+        print(f"starting upload {i}")
+        chunk_data = upload.read(chunk_size)
+        print(hash(chunk_data))
+        start_index = i * chunk_size 
+        end_index = start_index + chunk_size
+
+        if i == chunk_number:
+            end_index = start_index + chunk_leftover
+
+        print(f"{start_index}/{end_index}")
+        headers = {
+            "Content-Length" : f"{chunk_size}",
+            "Content-range" : f"bytes {start_index}-{end_index-1}/{total_file_size}"
+        }        
+
+        chunkl_data_upload_status = requests.put(upload_url,headers=headers,data=chunk_data)
+        speed_estimate = (chunk_size * i) / (time.time()-time_start) / 1024 / 1024
+        speed_estimate = round(speed_estimate, 2)
+        print(f"Upload Progress: {speed_estimate} MB/s {i}/{chunk_number}   {chunkl_data_upload_status}")
+        return chunkl_data_upload_status    
+    
+    except Exception as e:
+            print(e)
