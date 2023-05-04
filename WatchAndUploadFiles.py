5223d90b6382a33f9b73391de093a239120b65cf
Richard Smith <qpalymmm@gmail.com>
2023-05-04 17:48:23 +0200

    watch

--- WatchAndUploadFiles.py
+++ WatchAndUploadFiles.py
@@ -4,23 +4,24 @@ from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 from OneDrive import uploadFile
 
+
 def WatchPath(folder_path):
     class NewFileHandler(FileSystemEventHandler):
-        def on_created(self, event):
+        def on_moved(self, event):
             if not event.is_directory:
-                filename = event.src_path
+                filename = event.dest_path
                 name = os.path.basename(filename)
                 path = os.path.dirname(filename)
-                uploadFile(path, name)
-
-    def process_new_file(filename):
-        # Do whatever you need to do with the new file here
-        print(f"Processing new file: {filename}")
-
+                extension = os.path.splitext(filename)[1]
+                print(f"on_created {filename}")
+                if extension == ".jpg" or extension == ".png":
+                    uploadFile(path, name)
+                else:
+                    print(f"wrong extension: {extension}")   
 
     event_handler = NewFileHandler()
     observer = Observer()
-    observer.schedule(event_handler, path=folder_path, recursive=False)
+    observer.schedule(event_handler, path=folder_path, recursive=True)
     observer.start()
 
     try:
@@ -29,10 +30,4 @@ def WatchPath(folder_path):
     except KeyboardInterrupt:
         observer.stop()
     observer.join()
-
-if __name__ == "__main__":
-    def my_callback(filename):
-        process_new_file(filename)
-        # Call any other functions you need to process the new file here
-
-    watch_folder_for_new_files('/path/to/watched/folder/', my_callback)
+    
